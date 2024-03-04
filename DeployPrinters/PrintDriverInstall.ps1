d9b1d9ee99b89f2234c0501de7486563e9421f89
Rov <sam@cooked.beer>
2024-03-04 13:56:23 +1030

    Added comments to printer driver script

--- DeployPrinters/PrintDriverInstall.ps1
+++ DeployPrinters/PrintDriverInstall.ps1
@@ -1,3 +1,5 @@
+#script is packaged as an app (Intunewin) and deployed along with oemsetup.inf
 C:\Windows\SysNative\pnputil.exe /add-driver "$psscriptroot\x64\oemsetup.inf" /install
+# Leave a file behind so it can be used during detection    
         $VictoryFilePath = "C:\Temp\Driver.txt"
         "Driver!" | Out-File -FilePath $VictoryFilePath
